41c6ffc8c931b67c4be5720ed8f1bcf2e67be6f5
Amanda Avelino <amavlopes@gmail.com>
2025-05-21 20:41:23 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { PrimeNG } from 'primeng/config'
+
+import { ButtonModule } from 'primeng/button'
+
+@Component({
+    selector: 'pa-root',
+    imports: [RouterOutlet, ButtonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+})
+export class AppComponent {
+    title = 'Alocação de Professores'
+
+    constructor(private primeng: PrimeNG) {}
+
+    ngOnInit() {
+        this.primeng.ripple.set(true)
+    }
+}
