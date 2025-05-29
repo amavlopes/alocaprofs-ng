6de788b24514c65e8d385cffd2d2d55be22e5186
Amanda Avelino <amavlopes@gmail.com>
2025-05-29 06:00:32 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { PrimeNG } from 'primeng/config'
+
+import { ButtonModule } from 'primeng/button'
+
+@Component({
+    selector: 'pa-root',
+    imports: [RouterOutlet, ButtonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+})
+export class AppComponent {
+    constructor(private primeng: PrimeNG) {}
+
+    ngOnInit() {
+        this.primeng.ripple.set(true)
+    }
+}
